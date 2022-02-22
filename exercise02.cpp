75b5d60f1a758609d79200021ccc390f0bbdc398
IT21164408 <99886413+IT21164408@users.noreply.github.com>
2022-02-22 17:53:13 +0000

    IT21164408 - Tutorial 3

--- exercise02.cpp
+++ exercise02.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int volume(int height, int width, int length);
+int volume(int height, int width, int length); //declare the function
 
-int main() {
-    int box1Height, box1Width, box1Length;
+int main() 
+{
+    int box1Height, box1Width, box1Length; //declare variables
     int box2Height, box2Width, box2Length;
     int totalVolume, totalSurface;
     
-    cout << "Enter Box 1 Height : ";
+    cout << "Enter Box 1 Height : "; //getting user inputs
     cin >> box1Height;
     cout << "Enter Box 1 Width : ";
     cin >> box1Width;
     cout << "Enter Box 1 Length : ";
     cin >> box1Length;
     
-     cout << "Enter Box 2 Height : ";
+    cout << "Enter Box 2 Height : ";
     cin >> box2Height;
     cout << "Enter Box 2 Width : ";
     cin >> box2Width;
@@ -23,11 +24,15 @@ int main() {
     cin >> box2Length;
     
     totalVolume = volume(box1Height, box1Width, box1Length)
-             + volume(box2Height, box2Width, box2Length);
+             + volume(box2Height, box2Width, box2Length); //calculation
              
-    cout << "Volume of Box is " << totalVolume << endl;
+    cout << endl << "Volume of Box is " << totalVolume << endl; //printing the output
     
     return 0;
 }
 
 // Implement the Volume() function here
+int volume(int height, int width, int length)
+{
+  return height * width * length ; //calculating th evolume
+}
